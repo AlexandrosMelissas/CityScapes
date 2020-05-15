fa4a0bbb7ed89996b1c1d2825d325b22f59356fa
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-05-15 17:58:26 +0200

    Changes

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -5,16 +5,16 @@ $user = 'root';
 $db = 'city_scapes_db';
 $pass = 'root';
 
-if(gethostname()=='users.iee.ihu.gr') {
+if (gethostname()=='users.iee.ihu.gr') {
 
-    $mysqli = new mysqli($host, $user, $pass, $db, null, 'home/student/it/2015/it154582/mysql/run/mysql.sock')
+$mysqli = new mysqli($host, $user, $pass, $db, null, 'home/student/it/2015/it154582/mysql/run/mysql.sock')
 } else {
     $db = 'db_table';
     $pass = '';
     $mysqli = new mysqli($host,$user,$pass,$db);
 }
 
-if($mysqli->connect_errno) {
+if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ")" . $mysqli -> connect_error;
 }
 
