adc648a1f67766256d019efc83c6f1e410755bc7
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-05-15 18:49:10 +0200

    Changes

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -4,7 +4,9 @@ $host = 'localhost';
 $user = 'root';
 $db = 'city_scapes_db';
 $pass = 'root';
-$mysqli = ''
+$mysqli = null
+
+global $mysqli;
 
 if(gethostname()=='users.iee.ihu.gr'){
     $mysqli = new mysqli($host, $user, $pass, $db, null, '/home/student/it/2015/it154582/mysql/run/mysql.sock')
