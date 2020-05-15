f063c9fd6f483f9df713a5d31ab12979caf21118
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-05-15 18:32:13 +0200

    Changes

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -5,6 +5,6 @@ $user = 'root';
 $db = 'city_scapes_db';
 $pass = 'root';
 
-$mysqli = new mysqli($host, $user, $pass, $db, null, '../../mysql/run/mysql.sock')
+$mysqli = new mysqli($host, $user, $pass, $db)
 
 ?>
