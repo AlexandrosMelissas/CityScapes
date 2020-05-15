a8cba4eef195dedbc2d04480200001469f81225b
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-05-15 18:00:46 +0200

    Changes

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -7,13 +7,4 @@ $pass = 'root';
 
 $mysqli = new mysqli($host, $user, $pass, $db, null, 'home/student/it/2015/it154582/mysql/run/mysql.sock')
 
-
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ")" . $mysqli -> connect_error;
-}
-
-
-
-
-
 ?>
