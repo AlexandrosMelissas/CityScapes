ef13002a90825b9943abcf5600782d72f52d832f
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-05-15 17:59:15 +0200

    Changes

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -8,11 +8,8 @@ $pass = 'root';
 if (gethostname()=='users.iee.ihu.gr') {
 
 $mysqli = new mysqli($host, $user, $pass, $db, null, 'home/student/it/2015/it154582/mysql/run/mysql.sock')
-} else {
-    $db = 'db_table';
-    $pass = '';
-    $mysqli = new mysqli($host,$user,$pass,$db);
-}
+
+} 
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ")" . $mysqli -> connect_error;
