c0febf43035bd64426efc784a591a3b520270ec6
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-05-15 18:00:02 +0200

    Changes

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -5,11 +5,8 @@ $user = 'root';
 $db = 'city_scapes_db';
 $pass = 'root';
 
-if (gethostname()=='users.iee.ihu.gr') {
-
 $mysqli = new mysqli($host, $user, $pass, $db, null, 'home/student/it/2015/it154582/mysql/run/mysql.sock')
 
-} 
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ")" . $mysqli -> connect_error;
